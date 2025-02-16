e6e6111d1675190c73f04ce79d4d4991383a9b35
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-02-16 17:11:37 +0530

    "topbar completed"

--- FrontEnd/src/components/Common/Header.jsx
+++ FrontEnd/src/components/Common/Header.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
     <>
-    {/* Topbar */}
     <Topbar/>
-
-    
-    {/* Ṇavbar */}
+    <Navbar/>
     {/* Cart Drawer */}
-    
     
     
     
--- FrontEnd/src/components/Layout/Topbar.jsx
+++ FrontEnd/src/components/Layout/Topbar.jsx
@@ -9,8 +9,8 @@ const Topbar = () => {
     <>
     
     <div className='bg-[#ea3e0e] text-white'>
-        <div className='container mx-auto'>
-            <div>
+        <div className='container mx-auto flex justify-between items-center py-3 px-4'>
+            <div className='hidden md:flex items-center space-x-4'>
                 <a href="#" className='hover:text-gray-300'>
                 <TbBrandMeta className='h-5 w-5' />
                 </a>
@@ -20,8 +20,14 @@ const Topbar = () => {
                 </a>
 
                 <a href="#" className='hover:text-gray-300'>
-                <RiTwitterXLine className='h-5 w-5' />
+                <RiTwitterXLine className='h-4 w-4' />
                 </a>
+            </div>
+            <div className='text-sm text-center flex-grow'>
+              <span> We ship wordlwide - Fast and reliable shipping!</span>
+            </div>
+            <div className='text-sm hidden md:block'>
+              <a href="tel:+1234567890" className='hover:text-gray-300'> +1 (234) 567-890</a>
             </div>
         </div>
     </div>
