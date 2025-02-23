9ad4d18c4aca69bc903ecc4a631d1b0d3d7a74c2
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-02-23 21:45:43 +0530

    "footer completed"

--- FrontEnd/src/components/Layout/UserLayout.jsx
+++ FrontEnd/src/components/Layout/UserLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer'
 
 const UserLayout = () => {
   return (
@@ -9,6 +10,7 @@ const UserLayout = () => {
 
     {/* main content */}
     {/* Footer */}
+    <Footer/>
     
     
     </>
