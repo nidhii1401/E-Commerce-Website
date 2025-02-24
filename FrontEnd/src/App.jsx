2154de01b3d127c30be69c85dd4474e9ebe0cfec
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-02-24 11:04:36 +0530

    'home'

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,15 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import UserLayout from './components/Layout/UserLayout';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route  path = '/' element ={ <UserLayout/>} >{/* User Layout */} </Route>
-      <Route>{/* Admin Layout */}  </Route>
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route>{/* Admin Layout */} </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- FrontEnd/src/components/Layout/UserLayout.jsx
+++ FrontEnd/src/components/Layout/UserLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Common/Header'
 import Footer from '../Common/Footer'
+import { Outlet } from 'react-router-dom'
 
 const UserLayout = () => {
   return (
@@ -9,6 +10,9 @@ const UserLayout = () => {
     <Header/>
 
     {/* main content */}
+    <main>
+      <Outlet/>
+    </main>
     {/* Footer */}
     <Footer/>
     
