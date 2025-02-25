f232b7508ce3a12bc0f8016d99d2a49c2e44c8f6
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-02-25 13:33:01 +0530

    "main-page-completed"

--- FrontEnd/src/components/Layout/Hero.jsx
+++ FrontEnd/src/components/Layout/Hero.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import heroImg from '../../assets/rabbit-hero.webp'
+import React from "react";
+import image from "../../assets/rabbit-hero.webp";
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
-    <section className='relative'>
-        <img src={heroImg} alt="Rabbit"  className='w-full h-[400px] md:h-[600px] lg:[600px] object-cover'/>
-        <div className='absolute inset-0 flex items-center justify-center'>
-            <div className='text-center text-white p-6'>
-                <h1 className='text-4xl md:text-9xl font-bold tracking-tighter mb-4'>VACATION</h1>
-            </div>
-        </div>
-    </section>
-  )
-}
+    <div
+      className="relative flex flex-col items-center justify-center w-full h-screen bg-cover bg-center text-center text-white"
+      id="simg"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <h1 className="font-bold text-9xl ">
+        VACATION <br /> READY
+      </h1>
+      <p className="text-sm mt-2">
+        Explore our vacation ready outfits with fast world wide shopping
+      </p>
 
-export default Hero
+      <Link
+        to="#"
+        className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+      >
+        {" "}
+        Shop Now
+      </Link>
+    </div>
+  );
+};
 
+export default Hero;
