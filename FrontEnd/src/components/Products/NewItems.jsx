f8ea466def6dc70525e80aae2739fcf5166bf933
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-02-26 17:17:28 +0530

    "page-3"

--- FrontEnd/src/components/Products/NewItems.jsx
+++ FrontEnd/src/components/Products/NewItems.jsx
@@ -9,7 +9,7 @@ const NewItems = () => {
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(false);
   const [canScrollRight, setCanScrollRight] = useState(true);
-  const [CanScrollLeft, setCanScrollLeft] = useState(true);
+  const [CanScrollLeft, setCanScrollLeft] = useState(false);
 
   const newArrivals = [
     {
@@ -109,6 +109,27 @@ const NewItems = () => {
     },
   ];
 
+
+  const handleMouseDown = (e) =>{
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  }
+
+  const handleMouseUporLeave = (e) => {
+    setIsDragging(false);
+  }
+
+  const handleMouseMove = (e) => {
+    if(!isDragging) return;
+
+    const x = scrollRef.current.offsetLeft;
+    const walk = x - startX;
+
+    scrollRef.current.scrollLeft - scrollLeft - walk;
+
+  }
+
   const scroll = (direction) => {
     const scrollAmount = direction === "left" ? -300 : 300;
     scrollRef.current.scrollBy({ left: scrollAmount, behaviour: "smooth" });
@@ -128,18 +149,21 @@ const NewItems = () => {
       scrollLeft: container.scrollLeft,
       clientWidth: container.clientWidth,
       containerScrollWidth: container.scrollWidth,
+      offsetLeft: scrollRef.current.offsetLeft,
     });
   };
   useEffect(() => {
     const container = scrollRef.current;
     if (container) {
       container.addEventListener("scroll", updateScrollButtons);
-      // updateScrollButtons();
+      updateScrollButtons();
+      
+      return () => container.removeEventListener("scroll",updateScrollButtons )
     }
-  });
+  },[]);
 
   return (
-    <div>
+    <div className="py-16 px-8">
       <div className="container mx-auto text-center mb-10 relative">
         <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
         <p className="text-lg text-gray-600 mb-8">
@@ -151,14 +175,14 @@ const NewItems = () => {
           <button
             onClick={() => scroll("left")}
             disabled={!CanScrollLeft}
-            className={`p-2 rounded-full border ${CanScrollLeft} ? "bg-white text-black" : "bg-gray-200 text-gray-400 cursor-not-allowed" `}
+            className={`p-2 rounded-full border ${CanScrollLeft ? "bg-white text-black" : "bg-white text-black cursor-not-allowed"} `}
           >
             <FiChevronLeft className="text-lg" />
           </button>
           <button
           onClick={() => scroll("right")}
           disabled={!canScrollRight}
-            className={`p-2 rounded-full border ${canScrollRight} ? "bg-white text-black" : "bg-gray-200 text-gray-400 cursor-not-allowed" `}
+          className={`p-2 rounded-full border ${canScrollRight ? "bg-white text-black" : "bg-white text-black cursor-not-allowed"} `}
           >
             <FiChevronRight className="text-lg" />
           </button>
@@ -168,7 +192,11 @@ const NewItems = () => {
       {/* Image scroller */}
       <div
         ref={scrollRef}
-        className="container mx-auto overflow-x-scroll flex space-x-6 relative"
+        className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${isDragging ? "cursor-grabbing" : "cursor-grab"}`}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUporLeave}
+        onMouseLeave={handleMouseUporLeave}
       >
         {newArrivals.map((product) => (
           <div key={product._id} className="min-w-[30%] relative">
@@ -176,6 +204,7 @@ const NewItems = () => {
               src={product.images[0]?.url}
               alt={product.images[0]?.altText || product.name}
               className="w-full h-[500px] object-cover rounded-lg"
+              draggable="false"
             />
             <div className="absolute bottom-0 left-0 right-0 opacity-90 backdrop-blur-md text-white p-4 rounded-b-lg">
               <Link to="#" className="block">
