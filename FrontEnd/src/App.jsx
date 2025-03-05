ed6f271741b384d120f03232a46fb41403db6dc0
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-03-05 22:25:03 +0530

    '..'

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
+          <Route path="/login" index element={<Login/>} />
         </Route>
         <Route>{/* Admin Layout */} </Route>
       </Routes>
