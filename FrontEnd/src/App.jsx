38d3bf611bbd91ab26ed5c107aa905180163cc28
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-03-06 12:19:06 +0530

    'Orders-page'

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
           <Route path="login" element={<Login/>} />
-          <Route path="register" index element={<Register/>} />
+          <Route path="register" element={<Register/>} />
+          <Route path="profile" element={<Profile/>} />
 
         </Route>
         {/* <Route>Admin Layout </Route> */}
