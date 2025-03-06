d5d2ae12b49b5690fa443f852c0fe5ff70e430f4
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-03-06 10:48:16 +0530

    "Login Page"

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -6,10 +6,15 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("User Login:", {email, password})
+  }
+
   return (
     <div className="flex">
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-6">
-        <form className="w-full max-w-md bg-white rounded-lg border shadow-sm p-6">
+        <form onSubmit={handleSubmit} className="w-full max-w-md bg-white rounded-lg border shadow-sm p-6">
           <div className="flex justify-center mb-6">
             <h2 className="text-xl font-medium">Rabbit</h2>
           </div>
